86740de760eeeb4ef18b89bf6efe545747c2bdf5
Shubham Pawar <shubham.pawar@columbia.edu>
2018-06-10 14:00:10 -0700

    print the message to console log

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
+
+router.use(bodyParser.json());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res){
+  var data = req.body;
+  for(var i = 0; i < data.messages.length; i++){
+    var message = data.messages[i];
+    console.log(message.author + ": " + message.body);
+  }
+  res.send('Ok');
 });
 
 module.exports = router;
